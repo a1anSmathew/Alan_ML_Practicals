d363f3001edc3273830c39b9538c633c2f05722d
Alan S Mathew <alansmathewibab@gmail.com>
2025-03-16 18:51:14 +0530

    Add files via upload

--- lab5/Alan_Lab5_Exercise_2.py
+++ lab5/Alan_Lab5_Exercise_2.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 x_list = [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10]
 A_list = []
 for x in x_list:
-    A = 1/(1 + (2.781**x))
+    A = 1/(1 + (2.781**-x))
     A_list.append(A)
 
 plt.figure(figsize=(8, 5))  # Set the figure size
